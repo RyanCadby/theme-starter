c8f92cd4b815d140f84bc73b00cbfe2889e5b95c
Ryan Cadby <cadby.2@gmail.com>
2021-06-12 18:36:12 -0600

    add sass bootstrap defaults

--- functions.php
+++ functions.php
@@ -13,8 +13,8 @@ add_action('wp_enqueue_scripts', 'load_stylesheets');
 function loadjs()
 {   
     
-    wp_register_script('jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', '', 1, true);
-    wp_enqueue_script('jQuery');
+    // wp_register_script('jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', '', 1, true);
+    // wp_enqueue_script('jQuery');
     
     wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true);
     wp_enqueue_script('customjs');
