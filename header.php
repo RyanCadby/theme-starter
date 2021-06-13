1e646812f876dc6715c1c2fd900afc44d5109a8e
Ryan Cadby <cadby.2@gmail.com>
2021-06-12 19:46:44 -0600

    bootstrap menu and bootstrap scss infrastructure

--- header.php
+++ header.php
@@ -9,15 +9,18 @@
 
 
 <header class="sticky-top">
-   <div class="container">
-        <?php wp_nav_menu (
-
-            array(
-
-                'theme_location' => 'top-menu',
-                'menu_class' => 'navigation'
-            )
-
-        );?>
+    <div class="container nav-cont">
+        <nav>
+            <?php
+                $nav_args = array(
+                    'theme_location' => 'top-menu',
+                    'container' => 'div',
+                    'container_class' => 'navbar-link-container navbar-collapse collapse justify-content-end',
+                    'menu_class' => 'navbar-nav',
+                    'container_id' => 'navbarSupportedContent',
+                );
+                wp_nav_menu($nav_args);
+            ?>
+        </nav>
     </div>
 </header>
